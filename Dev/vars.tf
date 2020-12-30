5085c182650796d170fa465137f58ebfd888e10e
vrik123 <42827942+vrik123@users.noreply.github.com>
2020-12-30 17:26:39 +0530

    Update vars.tf

--- Dev/vars.tf
+++ Dev/vars.tf
@@ -2,8 +2,8 @@ variable "keyname"{
 default ="test"
 }
 variable "vpcid"{
-default ="vpc-03d92f7e"
+default ="vpc-03d9"
 }
 variable "ami_id"{
-default="ami-0739f8cdb239fe9ae"
+default="ami-0739f8cd"
 }

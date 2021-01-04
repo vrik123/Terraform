53a0d2cfed91f2ebf935e109f816c4bbb4fd2b7b
vrik123 <42827942+vrik123@users.noreply.github.com>
2021-01-04 13:25:09 +0530

    Update vars.tf

--- Dev/vars.tf
+++ Dev/vars.tf
@@ -5,5 +5,18 @@ variable "vpcid"{
 default ="vpc-03d9"
 }
 variable "ami_id"{
-default="ami-0739f8cd"
+default="ami-0739f8"
+}
+variable "web_ami_id"{
+default="ami-0885b1"
+}
+
+variable "tar_ami_id"{
+	default="ami-08"
+}
+variable "instance_count" {
+  default = "2"
+}
+variable "ansi_ami_id"{
+default="ami-0885b"	
 }
